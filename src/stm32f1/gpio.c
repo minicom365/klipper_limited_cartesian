cdp1279/klipper

stm32f1: Use enumerations for pin mappings
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/stm32f1/gpio.c
@@ -17,6 +17,12 @@
  * Pin mappings
  ****************************************************************/
 
+DECL_ENUMERATION_RANGE("pin", "PA", GPIO('A', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PB", GPIO('B', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PC", GPIO('C', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PD", GPIO('D', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PE", GPIO('E', 0), 16);
+
 GPIO_TypeDef *const digital_regs[] = {
     GPIOA, GPIOB, GPIOC, GPIOD, GPIOE
 };

minicom365/klipper_limited_cartesian

src/stm32f1/gpio.c
@@ -17,6 +17,12 @@
  * Pin mappings
  ****************************************************************/
 
+DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PC0", GPIO('C', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PD0", GPIO('D', 0), 16);
+DECL_ENUMERATION_RANGE("pin", "PE0", GPIO('E', 0), 16);
+
 GPIO_TypeDef *const digital_regs[] = {
     GPIOA, GPIOB, GPIOC, GPIOD, GPIOE
 };