cdp1279/klipper

pru: Use enumerations for pin mappings
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/pru/adc.c
@@ -51,6 +51,8 @@ adc_full_reset(void)
     have_done_reset = 1;
 }
 
+DECL_ENUMERATION_RANGE("pin", "AIN", 4 * 32, 8);
+
 struct gpio_adc
 gpio_adc_setup(uint8_t pin)
 {

src/pru/gpio.c
@@ -29,6 +29,11 @@ struct gpio_regs {
     volatile uint32_t setdataout;
 };
 
+DECL_ENUMERATION_RANGE("pin", "gpio0_", GPIO(0, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio1_", GPIO(1, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio2_", GPIO(2, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio3_", GPIO(3, 0), 32);
+
 static struct gpio_regs *digital_regs[] = {
     (void*)0x44e07000, (void*)0x4804c000, (void*)0x481ac000, (void*)0x481ae000
 };

minicom365/klipper_limited_cartesian

src/pru/adc.c
@@ -51,6 +51,8 @@ adc_full_reset(void)
     have_done_reset = 1;
 }
 
+DECL_ENUMERATION_RANGE("pin", "AIN0", 4 * 32, 8);
+
 struct gpio_adc
 gpio_adc_setup(uint8_t pin)
 {

src/pru/gpio.c
@@ -29,6 +29,11 @@ struct gpio_regs {
     volatile uint32_t setdataout;
 };
 
+DECL_ENUMERATION_RANGE("pin", "gpio0_0", GPIO(0, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio1_0", GPIO(1, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio2_0", GPIO(2, 0), 32);
+DECL_ENUMERATION_RANGE("pin", "gpio3_0", GPIO(3, 0), 32);
+
 static struct gpio_regs *digital_regs[] = {
     (void*)0x44e07000, (void*)0x4804c000, (void*)0x481ac000, (void*)0x481ae000
 };