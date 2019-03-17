cdp1279/klipper

atsamd: Use enumerations for pin mappings
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/atsamd/gpio.c
@@ -44,8 +44,14 @@ gpio_peripheral(uint32_t gpio, char ptype, int32_t pull_up)
 
 #if CONFIG_MACH_SAMD21
 #define NUM_PORT 2
+DECL_ENUMERATION_RANGE("pin", "PA", GPIO('A', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PB", GPIO('B', 0), 32);
 #elif CONFIG_MACH_SAMD51
 #define NUM_PORT 4
+DECL_ENUMERATION_RANGE("pin", "PA", GPIO('A', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PB", GPIO('B', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PC", GPIO('C', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PD", GPIO('D', 0), 32);
 #endif
 
 struct gpio_out

minicom365/klipper_limited_cartesian

src/atsamd/gpio.c
@@ -44,8 +44,14 @@ gpio_peripheral(uint32_t gpio, char ptype, int32_t pull_up)
 
 #if CONFIG_MACH_SAMD21
 #define NUM_PORT 2
+DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 32);
 #elif CONFIG_MACH_SAMD51
 #define NUM_PORT 4
+DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PC0", GPIO('C', 0), 32);
+DECL_ENUMERATION_RANGE("pin", "PD0", GPIO('D', 0), 32);
 #endif
 
 struct gpio_out