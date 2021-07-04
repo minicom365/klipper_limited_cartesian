pixeldoc2000/klipper

rp2040: Add initial adc support
Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/rp2040/adc.c
@@ -1,0 +1,88 @@
+// ADC functions on rp2040
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include "board/misc.h" // timer_from_us
+#include "command.h" // shutdown
+#include "gpio.h" // gpio_adc_setup
+#include "hardware/structs/adc.h" // adc_hw
+#include "hardware/structs/padsbank0.h" // padsbank0_hw
+#include "hardware/structs/resets.h" // RESETS_RESET_ADC_BITS
+#include "internal.h" // unreset
+#include "sched.h" // sched_shutdown
+
+DECL_CONSTANT("ADC_MAX", 4095);
+
+#define ADC_TEMPERATURE_PIN 0xfe
+DECL_ENUMERATION("pin", "ADC_TEMPERATURE", ADC_TEMPERATURE_PIN);
+
+struct gpio_adc
+gpio_adc_setup(uint32_t pin)
+{
+    if ((pin < 26 || pin > 29) && pin != ADC_TEMPERATURE_PIN)
+        shutdown("Not a valid ADC pin");
+
+    // Enable the ADC
+    if (is_in_reset(RESETS_RESET_ADC_BITS)) {
+        unreset(RESETS_RESET_ADC_BITS);
+        adc_hw->cs = ADC_CS_EN_BITS;
+    }
+
+    uint8_t chan;
+    if (pin == ADC_TEMPERATURE_PIN) {
+        chan = 4;
+        adc_hw->cs |= ADC_CS_TS_EN_BITS;
+    } else {
+        chan = pin - 26;
+        padsbank0_hw->io[pin] = PADS_BANK0_GPIO0_OD_BITS;
+    }
+
+    return (struct gpio_adc){ .chan = chan };
+}
+
+enum { ADC_DUMMY=0xff };
+static uint8_t last_analog_read = ADC_DUMMY;
+
+// Try to sample a value. Returns zero if sample ready, otherwise
+// returns the number of clock ticks the caller should wait before
+// retrying this function.
+uint32_t
+gpio_adc_sample(struct gpio_adc g)
+{
+    uint32_t cs = adc_hw->cs;
+    if (!(cs & ADC_CS_READY_BITS))
+        // ADC is busy
+        goto need_delay;
+    if (last_analog_read == g.chan)
+        // Sample now ready
+        return 0;
+    if (last_analog_read != ADC_DUMMY)
+        // Sample on another channel in progress
+        goto need_delay;
+
+    // Begin sample
+    last_analog_read = g.chan;
+    adc_hw->cs = ((cs & ADC_CS_TS_EN_BITS) | ADC_CS_START_ONCE_BITS
+                  | ADC_CS_EN_BITS | (g.chan << ADC_CS_AINSEL_LSB));
+
+need_delay:
+    return timer_from_us(5); // Sample takes 2us but provide extra time
+}
+
+// Read a value; use only after gpio_adc_sample() returns zero
+uint16_t
+gpio_adc_read(struct gpio_adc g)
+{
+    last_analog_read = ADC_DUMMY;
+    return adc_hw->result;
+}
+
+// Cancel a sample that may have been started with gpio_adc_sample()
+void
+gpio_adc_cancel_sample(struct gpio_adc g)
+{
+    if (last_analog_read == g.chan)
+        last_analog_read = ADC_DUMMY;
+}

src/rp2040/main.c
@@ -42,6 +42,7 @@ DECL_INIT(watchdog_init);
  ****************************************************************/
 
 #define FREQ_SYS 125000000
+#define FREQ_USB 48000000
 
 uint32_t
 get_pclock_frequency(void)
@@ -56,6 +57,12 @@ unreset(uint32_t reset_bit)
     resets_hw->reset &= ~reset_bit;
     while (!(resets_hw->reset_done & reset_bit))
         ;
+}
+
+int
+is_in_reset(uint32_t reset_bit)
+{
+    return resets_hw->reset & reset_bit;
 }
 
 static void
@@ -86,6 +93,14 @@ pll_setup(pll_hw_t *pll, uint32_t mul, uint32_t postdiv)
 }
 
 static void
+clk_aux_setup(uint32_t clk_id, uint32_t aux_id)
+{
+    clock_hw_t *clk = &clocks_hw->clk[clk_id];
+    clk->ctrl = 0;
+    clk->ctrl = aux_id | CLOCKS_CLK_PERI_CTRL_ENABLE_BITS;
+}
+
+static void
 clock_setup(void)
 {
     // Set clk_sys and clk_ref to use internal clock
@@ -112,12 +127,13 @@ clock_setup(void)
     while (!(csys->selected & (1 << 1)))
         ;
 
-    // Setup clk_peri
-    clock_hw_t *cperi = &clocks_hw->clk[clk_peri];
-    cperi->ctrl = 0;
-    cperi->ctrl = CLOCKS_CLK_PERI_CTRL_ENABLE_BITS;
-    while (!(cperi->selected & (1 << 0)))
-        ;
+    // Setup pll_usb
+    unreset(RESETS_RESET_PLL_USB_BITS);
+    pll_setup(pll_usb_hw, 40, 40*CONFIG_CLOCK_REF_FREQ/FREQ_USB);
+
+    // Setup clk_peri and clk_adc
+    clk_aux_setup(clk_peri, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS);
+    clk_aux_setup(clk_adc, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB);
 
     // Enable watchdog tick (at 12Mhz)
     cref->div = 1<<CLOCKS_CLK_REF_DIV_INT_LSB;

minicom365/klipper_limited_cartesian

src/rp2040/adc.c
@@ -1,0 +1,88 @@
+// ADC functions on rp2040
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include "board/misc.h" // timer_from_us
+#include "command.h" // shutdown
+#include "gpio.h" // gpio_adc_setup
+#include "hardware/structs/adc.h" // adc_hw
+#include "hardware/structs/padsbank0.h" // padsbank0_hw
+#include "hardware/structs/resets.h" // RESETS_RESET_ADC_BITS
+#include "internal.h" // enable_pclock
+#include "sched.h" // sched_shutdown
+
+DECL_CONSTANT("ADC_MAX", 4095);
+
+#define ADC_TEMPERATURE_PIN 0xfe
+DECL_ENUMERATION("pin", "ADC_TEMPERATURE", ADC_TEMPERATURE_PIN);
+
+struct gpio_adc
+gpio_adc_setup(uint32_t pin)
+{
+    if ((pin < 26 || pin > 29) && pin != ADC_TEMPERATURE_PIN)
+        shutdown("Not a valid ADC pin");
+
+    // Enable the ADC
+    if (!is_enabled_pclock(RESETS_RESET_ADC_BITS)) {
+        enable_pclock(RESETS_RESET_ADC_BITS);
+        adc_hw->cs = ADC_CS_EN_BITS;
+    }
+
+    uint8_t chan;
+    if (pin == ADC_TEMPERATURE_PIN) {
+        chan = 4;
+        adc_hw->cs |= ADC_CS_TS_EN_BITS;
+    } else {
+        chan = pin - 26;
+        padsbank0_hw->io[pin] = PADS_BANK0_GPIO0_OD_BITS;
+    }
+
+    return (struct gpio_adc){ .chan = chan };
+}
+
+enum { ADC_DUMMY=0xff };
+static uint8_t last_analog_read = ADC_DUMMY;
+
+// Try to sample a value. Returns zero if sample ready, otherwise
+// returns the number of clock ticks the caller should wait before
+// retrying this function.
+uint32_t
+gpio_adc_sample(struct gpio_adc g)
+{
+    uint32_t cs = adc_hw->cs;
+    if (!(cs & ADC_CS_READY_BITS))
+        // ADC is busy
+        goto need_delay;
+    if (last_analog_read == g.chan)
+        // Sample now ready
+        return 0;
+    if (last_analog_read != ADC_DUMMY)
+        // Sample on another channel in progress
+        goto need_delay;
+
+    // Begin sample
+    last_analog_read = g.chan;
+    adc_hw->cs = ((cs & ADC_CS_TS_EN_BITS) | ADC_CS_START_ONCE_BITS
+                  | ADC_CS_EN_BITS | (g.chan << ADC_CS_AINSEL_LSB));
+
+need_delay:
+    return timer_from_us(5); // Sample takes 2us but provide extra time
+}
+
+// Read a value; use only after gpio_adc_sample() returns zero
+uint16_t
+gpio_adc_read(struct gpio_adc g)
+{
+    last_analog_read = ADC_DUMMY;
+    return adc_hw->result;
+}
+
+// Cancel a sample that may have been started with gpio_adc_sample()
+void
+gpio_adc_cancel_sample(struct gpio_adc g)
+{
+    if (last_analog_read == g.chan)
+        last_analog_read = ADC_DUMMY;
+}

src/rp2040/main.c
@@ -42,6 +42,7 @@ DECL_INIT(watchdog_init);
 
 #define FREQ_XOSC 12000000
 #define FREQ_SYS 125000000
+#define FREQ_USB 48000000
 
 void
 enable_pclock(uint32_t reset_bit)
@@ -126,8 +127,13 @@ clock_setup(void)
     while (!(csys->selected & (1 << 1)))
         ;
 
-    // Setup clk_peri
+    // Setup pll_usb
+    enable_pclock(RESETS_RESET_PLL_USB_BITS);
+    pll_setup(pll_usb_hw, 40, 40*FREQ_XOSC/FREQ_USB);
+
+    // Setup clk_peri and clk_adc
     clk_aux_setup(clk_peri, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS);
+    clk_aux_setup(clk_adc, CLOCKS_CLK_ADC_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB);
 
     // Enable watchdog tick (at 12Mhz)
     cref->div = 1<<CLOCKS_CLK_REF_DIV_INT_LSB;