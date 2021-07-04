TheFanatr/klipper

rp2040: Add initial support for the rp2040 mcu
Support the rp2040 (as tested on a Raspberry Pi Pico board).  This
adds basic uart, timer, gpio, and watchdog support.

Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

src/rp2040/main.c
@@ -1,0 +1,139 @@
+// Startup code on rp2040
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include <stdint.h> // uint32_t
+#include "autoconf.h" // CONFIG_CLOCK_REF_FREQ
+#include "hardware/structs/clocks.h" // clock_hw_t
+#include "hardware/structs/pll.h" // pll_hw_t
+#include "hardware/structs/resets.h" // sio_hw
+#include "hardware/structs/watchdog.h" // watchdog_hw
+#include "hardware/structs/xosc.h" // xosc_hw
+#include "sched.h" // sched_main
+
+
+/****************************************************************
+ * watchdog handler
+ ****************************************************************/
+
+void
+watchdog_reset(void)
+{
+    watchdog_hw->load = 0x800000;
+}
+DECL_TASK(watchdog_reset);
+
+void
+watchdog_init(void)
+{
+    watchdog_reset();
+    watchdog_hw->ctrl = (WATCHDOG_CTRL_PAUSE_DBG0_BITS
+                         | WATCHDOG_CTRL_PAUSE_DBG1_BITS
+                         | WATCHDOG_CTRL_PAUSE_JTAG_BITS
+                         | WATCHDOG_CTRL_ENABLE_BITS);
+}
+DECL_INIT(watchdog_init);
+
+
+/****************************************************************
+ * Clock setup
+ ****************************************************************/
+
+#define FREQ_SYS 125000000
+
+uint32_t
+get_pclock_frequency(void)
+{
+    return FREQ_SYS;
+}
+
+void
+unreset(uint32_t reset_bit)
+{
+    resets_hw->reset |= reset_bit;
+    resets_hw->reset &= ~reset_bit;
+    while (!(resets_hw->reset_done & reset_bit))
+        ;
+}
+
+static void
+xosc_setup(void)
+{
+    xosc_hw->startup = DIV_ROUND_UP(CONFIG_CLOCK_REF_FREQ, 1000 * 256); // 1ms
+    xosc_hw->ctrl = (XOSC_CTRL_FREQ_RANGE_VALUE_1_15MHZ
+                     | (XOSC_CTRL_ENABLE_VALUE_ENABLE << XOSC_CTRL_ENABLE_LSB));
+    while(!(xosc_hw->status & XOSC_STATUS_STABLE_BITS))
+        ;
+}
+
+static void
+pll_setup(pll_hw_t *pll, uint32_t mul, uint32_t postdiv)
+{
+    // Setup pll
+    uint32_t refdiv = 1, fbdiv = mul, postdiv2 = 2, postdiv1 = postdiv/postdiv2;
+    pll->cs = refdiv;
+    pll->fbdiv_int = fbdiv;
+    pll->pwr = PLL_PWR_DSMPD_BITS | PLL_PWR_POSTDIVPD_BITS;
+    while (!(pll->cs & PLL_CS_LOCK_BITS))
+        ;
+
+    // Setup post divider
+    pll->prim = ((postdiv1 << PLL_PRIM_POSTDIV1_LSB)
+                 | (postdiv2 << PLL_PRIM_POSTDIV2_LSB));
+    pll->pwr = PLL_PWR_DSMPD_BITS;
+}
+
+static void
+clock_setup(void)
+{
+    // Set clk_sys and clk_ref to use internal clock
+    clock_hw_t *csys = &clocks_hw->clk[clk_sys];
+    csys->ctrl &= ~CLOCKS_CLK_SYS_CTRL_SRC_BITS;
+    while (csys->selected != 0x1)
+        ;
+    clock_hw_t *cref = &clocks_hw->clk[clk_ref];
+    cref->ctrl &= ~CLOCKS_CLK_REF_CTRL_SRC_BITS;
+    while (cref->selected != 0x1)
+        ;
+
+    // Reset peripherals (that can be)
+    resets_hw->reset = ~(RESETS_RESET_IO_QSPI_BITS
+                         | RESETS_RESET_PADS_QSPI_BITS);
+
+    // Setup xosc, pll_sys, and switch clk_sys
+    xosc_setup();
+    unreset(RESETS_RESET_PLL_SYS_BITS);
+    pll_setup(pll_sys_hw, 125, 125*CONFIG_CLOCK_REF_FREQ/FREQ_SYS);
+    csys->ctrl = 0;
+    csys->div = 1<<CLOCKS_CLK_SYS_DIV_INT_LSB;
+    csys->ctrl = CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX;
+    while (!(csys->selected & (1 << 1)))
+        ;
+
+    // Setup clk_peri
+    clock_hw_t *cperi = &clocks_hw->clk[clk_peri];
+    cperi->ctrl = 0;
+    cperi->ctrl = CLOCKS_CLK_PERI_CTRL_ENABLE_BITS;
+    while (!(cperi->selected & (1 << 0)))
+        ;
+
+    // Enable watchdog tick (at 12Mhz)
+    cref->div = 1<<CLOCKS_CLK_REF_DIV_INT_LSB;
+    cref->ctrl = CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC;
+    while (!(cref->selected & (1 << 2)))
+        ;
+    watchdog_hw->tick = 1 | WATCHDOG_TICK_ENABLE_BITS;
+
+    // Enable GPIO control
+    unreset(RESETS_RESET_IO_BANK0_BITS | RESETS_RESET_PADS_BANK0_BITS);
+}
+
+// Main entry point - called from armcm_boot.c:ResetHandler()
+void
+armcm_main(void)
+{
+    clock_setup();
+    sched_main();
+}

src/rp2040/serial.c
@@ -1,0 +1,89 @@
+// rp2040 serial
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include <stdint.h> // uint32_t
+#include "autoconf.h" // CONFIG_SERIAL
+#include "board/armcm_boot.h" // armcm_enable_irq
+#include "board/irq.h" // irq_save
+#include "board/serial_irq.h" // serial_rx_data
+#include "hardware/structs/resets.h" // RESETS_RESET_UART0_BITS
+#include "hardware/structs/uart.h" // UART0_BASE
+#include "internal.h" // UART0_IRQn
+#include "sched.h" // DECL_INIT
+
+#define UARTx uart0_hw
+#define UARTx_IRQn UART0_IRQ_IRQn
+#define GPIO_Rx 1
+#define GPIO_Tx 0
+
+// Write tx bytes to the serial port
+static void
+kick_tx(void)
+{
+    for (;;) {
+        if (UARTx->fr & UART_UARTFR_TXFF_BITS) {
+            // Output fifo full - enable tx irq
+            UARTx->imsc = (UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS
+                           | UART_UARTIMSC_TXIM_BITS);
+            break;
+        }
+        uint8_t data;
+        int ret = serial_get_tx_byte(&data);
+        if (ret) {
+            // No more data to send - disable tx irq
+            UARTx->imsc = UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS;
+            break;
+        }
+        UARTx->dr = data;
+    }
+}
+
+void
+UARTx_IRQHandler(void)
+{
+    uint32_t mis = UARTx->mis;
+    if (mis & (UART_UARTMIS_RXMIS_BITS | UART_UARTMIS_RTMIS_BITS))
+        serial_rx_byte(UARTx->dr);
+    else if (mis & UART_UARTMIS_TXMIS_BITS)
+        kick_tx();
+}
+
+void
+serial_enable_tx_irq(void)
+{
+    if (!(UARTx->fr & UART_UARTFR_TXFF_BITS)) {
+        irqstatus_t flag = irq_save();
+        kick_tx();
+        irq_restore(flag);
+    }
+}
+
+void
+serial_init(void)
+{
+    unreset(RESETS_RESET_UART0_BITS);
+
+    // Setup baud
+    uint32_t pclk = get_pclock_frequency();
+    uint32_t div = DIV_ROUND_CLOSEST(pclk * 4, CONFIG_SERIAL_BAUD);
+    UARTx->ibrd = div >> 6;
+    UARTx->fbrd = div & 0x3f;
+
+    // Enable fifo, set 8N1
+    UARTx->lcr_h = UART_UARTLCR_H_FEN_BITS | UART_UARTLCR_H_WLEN_BITS;
+    UARTx->ifls = 0;
+    UARTx->cr = (UART_UARTCR_RXE_BITS | UART_UARTCR_TXE_BITS
+                 | UART_UARTCR_UARTEN_BITS);
+
+    // Setup pins
+    gpio_peripheral(GPIO_Rx, 2, 1);
+    gpio_peripheral(GPIO_Tx, 2, 0);
+
+    // Enable receive irq
+    armcm_enable_irq(UARTx_IRQHandler, UARTx_IRQn, 0);
+    UARTx->imsc = UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS;
+}
+DECL_INIT(serial_init);

src/rp2040/timer.c
@@ -1,0 +1,70 @@
+// rp2040 timer support
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include "board/armcm_boot.h" // armcm_enable_irq
+#include "board/irq.h" // irq_disable
+#include "board/misc.h" // timer_read_time
+#include "board/timer_irq.h" // timer_dispatch_many
+#include "command.h" // DECL_SHUTDOWN
+#include "hardware/structs/resets.h" // RESETS_RESET_UART0_BITS
+#include "hardware/structs/timer.h" // RESETS_RESET_UART0_BITS
+#include "internal.h" // unreset
+#include "sched.h" // DECL_INIT
+
+
+/****************************************************************
+ * Low level timer code
+ ****************************************************************/
+
+// Return the current time (in absolute clock ticks).
+uint32_t
+timer_read_time(void)
+{
+    return timer_hw->timerawl;
+}
+
+static inline void
+timer_set(uint32_t next)
+{
+    timer_hw->alarm[0] = next;
+}
+
+// Activate timer dispatch as soon as possible
+void
+timer_kick(void)
+{
+    timer_set(timer_read_time() + 5);
+}
+
+
+/****************************************************************
+ * Setup and irqs
+ ****************************************************************/
+
+// Hardware timer IRQ handler - dispatch software timers
+void __aligned(16)
+TIMER0_IRQHandler(void)
+{
+    irq_disable();
+    uint32_t next = timer_dispatch_many();
+    timer_set(next);
+    timer_hw->intr = 1;
+    irq_enable();
+}
+
+void
+timer_init(void)
+{
+    irq_disable();
+    unreset(RESETS_RESET_TIMER_BITS);
+    timer_hw->timelw = 0;
+    timer_hw->timehw = 0;
+    armcm_enable_irq(TIMER0_IRQHandler, TIMER_IRQ_0_IRQn, 2);
+    timer_hw->inte = 1;
+    timer_kick();
+    irq_enable();
+}
+DECL_INIT(timer_init);

minicom365/klipper_limited_cartesian

src/rp2040/main.c
@@ -1,0 +1,149 @@
+// Startup code on rp2040
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include <stdint.h> // uint32_t
+#include "hardware/structs/clocks.h" // clock_hw_t
+#include "hardware/structs/pll.h" // pll_hw_t
+#include "hardware/structs/resets.h" // sio_hw
+#include "hardware/structs/watchdog.h" // watchdog_hw
+#include "hardware/structs/xosc.h" // xosc_hw
+#include "sched.h" // sched_main
+
+
+/****************************************************************
+ * watchdog handler
+ ****************************************************************/
+
+void
+watchdog_reset(void)
+{
+    watchdog_hw->load = 0x800000; // ~350ms
+}
+DECL_TASK(watchdog_reset);
+
+void
+watchdog_init(void)
+{
+    watchdog_reset();
+    watchdog_hw->ctrl = (WATCHDOG_CTRL_PAUSE_DBG0_BITS
+                         | WATCHDOG_CTRL_PAUSE_DBG1_BITS
+                         | WATCHDOG_CTRL_PAUSE_JTAG_BITS
+                         | WATCHDOG_CTRL_ENABLE_BITS);
+}
+DECL_INIT(watchdog_init);
+
+
+/****************************************************************
+ * Clock setup
+ ****************************************************************/
+
+#define FREQ_XOSC 12000000
+#define FREQ_SYS 125000000
+
+void
+enable_pclock(uint32_t reset_bit)
+{
+    resets_hw->reset |= reset_bit;
+    resets_hw->reset &= ~reset_bit;
+    while (!(resets_hw->reset_done & reset_bit))
+        ;
+}
+
+int
+is_enabled_pclock(uint32_t reset_bit)
+{
+    return !(resets_hw->reset & reset_bit);
+}
+
+uint32_t
+get_pclock_frequency(uint32_t reset_bit)
+{
+    return FREQ_SYS;
+}
+
+static void
+xosc_setup(void)
+{
+    xosc_hw->startup = DIV_ROUND_UP(FREQ_XOSC, 1000 * 256); // 1ms
+    xosc_hw->ctrl = (XOSC_CTRL_FREQ_RANGE_VALUE_1_15MHZ
+                     | (XOSC_CTRL_ENABLE_VALUE_ENABLE << XOSC_CTRL_ENABLE_LSB));
+    while(!(xosc_hw->status & XOSC_STATUS_STABLE_BITS))
+        ;
+}
+
+static void
+pll_setup(pll_hw_t *pll, uint32_t mul, uint32_t postdiv)
+{
+    // Setup pll
+    uint32_t refdiv = 1, fbdiv = mul, postdiv2 = 2, postdiv1 = postdiv/postdiv2;
+    pll->cs = refdiv;
+    pll->fbdiv_int = fbdiv;
+    pll->pwr = PLL_PWR_DSMPD_BITS | PLL_PWR_POSTDIVPD_BITS;
+    while (!(pll->cs & PLL_CS_LOCK_BITS))
+        ;
+
+    // Setup post divider
+    pll->prim = ((postdiv1 << PLL_PRIM_POSTDIV1_LSB)
+                 | (postdiv2 << PLL_PRIM_POSTDIV2_LSB));
+    pll->pwr = PLL_PWR_DSMPD_BITS;
+}
+
+static void
+clk_aux_setup(uint32_t clk_id, uint32_t aux_id)
+{
+    clock_hw_t *clk = &clocks_hw->clk[clk_id];
+    clk->ctrl = 0;
+    clk->ctrl = aux_id | CLOCKS_CLK_PERI_CTRL_ENABLE_BITS;
+}
+
+static void
+clock_setup(void)
+{
+    // Set clk_sys and clk_ref to use internal clock
+    clock_hw_t *csys = &clocks_hw->clk[clk_sys];
+    csys->ctrl &= ~CLOCKS_CLK_SYS_CTRL_SRC_BITS;
+    while (csys->selected != 0x1)
+        ;
+    clock_hw_t *cref = &clocks_hw->clk[clk_ref];
+    cref->ctrl &= ~CLOCKS_CLK_REF_CTRL_SRC_BITS;
+    while (cref->selected != 0x1)
+        ;
+
+    // Reset peripherals (that can be)
+    resets_hw->reset = ~(RESETS_RESET_IO_QSPI_BITS
+                         | RESETS_RESET_PADS_QSPI_BITS);
+
+    // Setup xosc, pll_sys, and switch clk_sys
+    xosc_setup();
+    enable_pclock(RESETS_RESET_PLL_SYS_BITS);
+    pll_setup(pll_sys_hw, 125, 125*FREQ_XOSC/FREQ_SYS);
+    csys->ctrl = 0;
+    csys->div = 1<<CLOCKS_CLK_SYS_DIV_INT_LSB;
+    csys->ctrl = CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLKSRC_CLK_SYS_AUX;
+    while (!(csys->selected & (1 << 1)))
+        ;
+
+    // Setup clk_peri
+    clk_aux_setup(clk_peri, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS);
+
+    // Enable watchdog tick (at 12Mhz)
+    cref->div = 1<<CLOCKS_CLK_REF_DIV_INT_LSB;
+    cref->ctrl = CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC;
+    while (!(cref->selected & (1 << 2)))
+        ;
+    watchdog_hw->tick = 1 | WATCHDOG_TICK_ENABLE_BITS;
+
+    // Enable GPIO control
+    enable_pclock(RESETS_RESET_IO_BANK0_BITS | RESETS_RESET_PADS_BANK0_BITS);
+}
+
+// Main entry point - called from armcm_boot.c:ResetHandler()
+void
+armcm_main(void)
+{
+    clock_setup();
+    sched_main();
+}

src/rp2040/serial.c
@@ -1,0 +1,92 @@
+// rp2040 serial
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include <stdint.h> // uint32_t
+#include "autoconf.h" // CONFIG_SERIAL
+#include "board/armcm_boot.h" // armcm_enable_irq
+#include "board/irq.h" // irq_save
+#include "board/serial_irq.h" // serial_rx_data
+#include "hardware/structs/resets.h" // RESETS_RESET_UART0_BITS
+#include "hardware/structs/uart.h" // UART0_BASE
+#include "internal.h" // UART0_IRQn
+#include "sched.h" // DECL_INIT
+
+#define UARTx uart0_hw
+#define UARTx_IRQn UART0_IRQ_IRQn
+#define GPIO_Rx 1
+#define GPIO_Tx 0
+
+// Write tx bytes to the serial port
+static void
+kick_tx(void)
+{
+    for (;;) {
+        if (UARTx->fr & UART_UARTFR_TXFF_BITS) {
+            // Output fifo full - enable tx irq
+            UARTx->imsc = (UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS
+                           | UART_UARTIMSC_TXIM_BITS);
+            break;
+        }
+        uint8_t data;
+        int ret = serial_get_tx_byte(&data);
+        if (ret) {
+            // No more data to send - disable tx irq
+            UARTx->imsc = UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS;
+            break;
+        }
+        UARTx->dr = data;
+    }
+}
+
+void
+UARTx_IRQHandler(void)
+{
+    uint32_t mis = UARTx->mis;
+    if (mis & (UART_UARTMIS_RXMIS_BITS | UART_UARTMIS_RTMIS_BITS)) {
+        do {
+            serial_rx_byte(UARTx->dr);
+        } while (!(UARTx->fr & UART_UARTFR_RXFE_BITS));
+    } else if (mis & UART_UARTMIS_TXMIS_BITS) {
+        kick_tx();
+    }
+}
+
+void
+serial_enable_tx_irq(void)
+{
+    if (!(UARTx->fr & UART_UARTFR_TXFF_BITS)) {
+        irqstatus_t flag = irq_save();
+        kick_tx();
+        irq_restore(flag);
+    }
+}
+
+void
+serial_init(void)
+{
+    enable_pclock(RESETS_RESET_UART0_BITS);
+
+    // Setup baud
+    uint32_t pclk = get_pclock_frequency(RESETS_RESET_UART0_BITS);
+    uint32_t div = DIV_ROUND_CLOSEST(pclk * 4, CONFIG_SERIAL_BAUD);
+    UARTx->ibrd = div >> 6;
+    UARTx->fbrd = div & 0x3f;
+
+    // Enable fifo, set 8N1
+    UARTx->lcr_h = UART_UARTLCR_H_FEN_BITS | UART_UARTLCR_H_WLEN_BITS;
+    UARTx->ifls = 0;
+    UARTx->cr = (UART_UARTCR_RXE_BITS | UART_UARTCR_TXE_BITS
+                 | UART_UARTCR_UARTEN_BITS);
+
+    // Setup pins
+    gpio_peripheral(GPIO_Rx, 2, 1);
+    gpio_peripheral(GPIO_Tx, 2, 0);
+
+    // Enable receive irq
+    armcm_enable_irq(UARTx_IRQHandler, UARTx_IRQn, 0);
+    UARTx->imsc = UART_UARTIMSC_RXIM_BITS | UART_UARTIMSC_RTIM_BITS;
+}
+DECL_INIT(serial_init);

src/rp2040/timer.c
@@ -1,0 +1,70 @@
+// rp2040 timer support
+//
+// Copyright (C) 2021  Kevin O'Connor <kevin@koconnor.net>
+//
+// This file may be distributed under the terms of the GNU GPLv3 license.
+
+#include "board/armcm_boot.h" // armcm_enable_irq
+#include "board/irq.h" // irq_disable
+#include "board/misc.h" // timer_read_time
+#include "board/timer_irq.h" // timer_dispatch_many
+#include "command.h" // DECL_SHUTDOWN
+#include "hardware/structs/resets.h" // RESETS_RESET_UART0_BITS
+#include "hardware/structs/timer.h" // RESETS_RESET_UART0_BITS
+#include "internal.h" // enable_pclock
+#include "sched.h" // DECL_INIT
+
+
+/****************************************************************
+ * Low level timer code
+ ****************************************************************/
+
+// Return the current time (in absolute clock ticks).
+uint32_t
+timer_read_time(void)
+{
+    return timer_hw->timerawl;
+}
+
+static inline void
+timer_set(uint32_t next)
+{
+    timer_hw->alarm[0] = next;
+}
+
+// Activate timer dispatch as soon as possible
+void
+timer_kick(void)
+{
+    timer_set(timer_read_time() + 50);
+}
+
+
+/****************************************************************
+ * Setup and irqs
+ ****************************************************************/
+
+// Hardware timer IRQ handler - dispatch software timers
+void __aligned(16)
+TIMER0_IRQHandler(void)
+{
+    irq_disable();
+    timer_hw->intr = 1;
+    uint32_t next = timer_dispatch_many();
+    timer_set(next);
+    irq_enable();
+}
+
+void
+timer_init(void)
+{
+    irq_disable();
+    enable_pclock(RESETS_RESET_TIMER_BITS);
+    timer_hw->timelw = 0;
+    timer_hw->timehw = 0;
+    armcm_enable_irq(TIMER0_IRQHandler, TIMER_IRQ_0_IRQn, 2);
+    timer_hw->inte = 1;
+    timer_kick();
+    irq_enable();
+}
+DECL_INIT(timer_init);