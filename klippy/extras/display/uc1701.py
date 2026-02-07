Klipper3d/klipper

bus: allow mark i2c as write only
If the bus is write only, with new i2c_transfer code
it is possible to at least provide some feedback on error

Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

klippy/extras/display/uc1701.py
@@ -130,7 +130,8 @@ class SPI4wire:
 class I2C:
     def __init__(self, config, default_addr):
         self.i2c = bus.MCU_I2C_from_config(config, default_addr=default_addr,
-                                           default_speed=400000)
+                                           default_speed=400000,
+                                           write_only=True)
     def send(self, cmds, is_data=False):
         if is_data:
             hdr = 0x40

minicom365/klipper_limited_cartesian

klippy/extras/display/uc1701.py
@@ -130,7 +130,8 @@ class SPI4wire:
 class I2C:
     def __init__(self, config, default_addr):
         self.i2c = bus.MCU_I2C_from_config(config, default_addr=default_addr,
-                                           default_speed=400000)
+                                           default_speed=400000,
+                                           async_write_only=True)
     def send(self, cmds, is_data=False):
         if is_data:
             hdr = 0x40